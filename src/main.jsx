70e0b3e66aa30bc89867576221261304071d6eb3
rlebhar-clone <robin.lebhar@getclone.io>
2024-07-11 15:39:26 +0200

    boiler plate

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { App } from "./App.jsx";
 import "./index.css";
+import { App } from "./App";
+import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export const Home = () => {
+  return (
+
+    <Box>Vite boiler plate is working</Box>
+
+  );
+};
